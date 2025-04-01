b948db63bf875884dced6516f710508becdeaa0c
AntoanNikolov <antoanameya@gmail.com>
2025-04-01 17:23:57 -0400

    Made models.py

--- models.py
+++ models.py
@@ -4,36 +4,11 @@ from django.conf import settings
 
 # Create your models here.
 
-#add stuff to user model
-
-class Reservation(models.Model):
-    username = models.CharField(max_length=200)
-    time_and_date = models.DateTimeField(auto_now=True) 
-
-    event = models.ForeignKey(Event, on_delete=models.CASCADE)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-
-    def __str__(self):
-       return f"Reservation for {self.event} by {self.user}"
-
 class Service(models.Model):
     name = models.CharField(max_length=200)
     service_description = models.CharField(max_length=200)
     def __str__(self):
         return self.name
-
-
-class Event(models.Model):
-    name = models.CharField(max_length=200)
-    description = models.CharField(max_length=200)
-    start_time_and_date = models.DateTimeField()
-    event_location = models.CharField(max_length=200)
-    end_time = models.DateTimeField()
-
-    service = models.ForeignKey(Service, on_delete=models.CASCADE)
-    def __str__(self):
-        return self.name
-
 
 class ServiceProfessional(models.Model):
     name = models.CharField(max_length=200)
@@ -42,4 +17,29 @@ class ServiceProfessional(models.Model):
     service = models.ManyToManyField(Service)
     def __str__(self):
         return self.name
+
+class Event(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    start_time_and_date = models.DateTimeField()
+    event_location = models.CharField(max_length=200)
+    end_time = models.DateTimeField()
+
+    #each event belongs to a service, a service may have many events under it
+    service = models.ForeignKey(Service, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
+
+class Reservation(models.Model):
+    username = models.CharField(max_length=200)
+    time_and_date = models.DateTimeField(auto_now=True) 
+
+    #each reservation belongs to an event, an event can have many reservations
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+       return f"Reservation for {self.event} by {self.user}"
+
+
 
