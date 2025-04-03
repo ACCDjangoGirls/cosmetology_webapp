018ea4dae956cbeeed94f386cb0ef895718ca93e
Giselle <gisellevi002@gmail.com>
2025-04-03 12:23:20 -0400

    finished 1st assignment

--- cosmetology/cosmetology/settings.py
+++ cosmetology/cosmetology/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'core.app.CosmetologyConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -117,6 +116,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+     BASE_DIR / "static",
+ ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
