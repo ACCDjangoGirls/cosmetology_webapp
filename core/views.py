780ea08c317f0854b8b88427f23f1402d0fa63a6
AntoanNikolov <antoanameya@gmail.com>
2025-04-11 15:01:48 -0400

    Progress

--- core/views.py
+++ core/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse, reverse_lazy
+from django.views import generic, View
+from .models import Service, ServiceProfessional, Event, Reservation, 
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
+
+class EventDetail(generic.DetailView):
+    model = Event
+
+class EventAdd(generic.CreateView):
+    model = Event
+
+class EventDelete(generic.DeleteView):
+    model = Event
+
+class EventEdit(generic.EditView):
+    model = Event
