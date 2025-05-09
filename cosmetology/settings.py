07976bbf58c224d8634a4185917512fc56533a3b
Chris Jones <christopher.jones@apsva.us>
2025-05-09 12:32:32 -0400

    initialize a variable

--- cosmetology/settings.py
+++ cosmetology/settings.py
@@ -15,6 +15,7 @@ import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+USE_POSTGRES = False
 
 env = environ.Env(
        DEBUG=(bool,False)
