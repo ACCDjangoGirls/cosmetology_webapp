d181d9d83a5857aef853d45277b11f663d4d9904
Chris <cgdanger17@gmail.com>
2025-05-13 12:20:59 -0400

    Model updated, fixture done

--- core/models.py
+++ core/models.py
@@ -7,7 +7,6 @@ from django.conf import settings
 class Service(models.Model):
     name = models.CharField(max_length=200)
     service_description = models.CharField(max_length=200)
-    price = models.IntegerField(null=True, blank=True)
     def __str__(self):
         return self.name
 
